18496611f26333bd2f5867e099cde9d39299738a
Sam Turner <sam.turner@kokadjo.net>
2025-05-21 16:15:04 -0400

    added update functionality w/o refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [enteredPayerName, setEnteredPlayerName] = useState();
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+
+  function handleChange(event) {
+    setIsSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setIsSubmitted(true);
+
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { isSubmitted ? enteredPayerName : 'unknown entity' }</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );

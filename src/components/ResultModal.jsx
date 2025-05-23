a72bba6ada436ab4ed8eb26910ae222ccf6c54dd
Sam Turner <sam.turner@kokadjo.net>
2025-05-22 20:30:48 -0400

    created modal form

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,6 @@
-export default function ResultModal({ result, targetTime }) {
+export default function ResultModal({ ref, result, targetTime }) {
     return (
-        <dialog className="result-modal" open>
+        <dialog ref={ref} className="result-modal">
             <h2>You {result}</h2>
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong> seconds left.</strong></p>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -6,11 +6,13 @@ export default function TimerChallenge({ title, targetTime }) {
     const [timerExpired, setTimerExpired] = useState(false);
 
     const timer = useRef();
+    const dialog = useRef();
 
 
     function handleStart() {
         timer.current = setTimeout(() => {
             setTimerExpired(true);
+            dialog.current.showModal();
         }, targetTime * 1000);
 
         setTimerStarted(true);
@@ -24,12 +26,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
     return (
         <>
-        {timerExpired && <ResultModal result={'lost'} targetTime={targetTime} /> }
+            <ResultModal ref={dialog} result={'lost'} targetTime={targetTime} />
             <section className="challenge">
                 <h2>{title}</h2>
-                
+
                 <p className="challeng-time">
-                   {targetTime} second{targetTime > 1 ? 's' : ''}
+                    {targetTime} second{targetTime > 1 ? 's' : ''}
                 </p>
                 <p>
                     <button onClick={timerStarted ? handleStop : handleStart}>

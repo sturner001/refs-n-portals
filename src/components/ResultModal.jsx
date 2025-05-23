cc1a7f420e5778b85b5bf6f373da4a98c2c5c990
Sam Turner <sam.turner@kokadjo.net>
2025-05-22 20:41:13 -0400

    forward ref example

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,6 @@
-export default function ResultModal({ ref, result, targetTime }) {
+import { forwardRef } from 'react';
+
+const ResultModal = forwardRef(function ResultModal({ result, targetTime}, ref) {
     return (
         <dialog ref={ref} className="result-modal">
             <h2>You {result}</h2>
@@ -9,4 +11,6 @@ export default function ResultModal({ ref, result, targetTime }) {
             </form>
         </dialog>
     );
-}
+});
+
+export default ResultModal;
